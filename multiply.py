b9275b5c45a5363fc0e41c85bee6ed1df0122c03
unknown <mayankyad>
2022-04-19 19:30:43 -0700

    added multiply

---
+++ multiply.py
@@ -0,0 +1,9 @@
+def mul(a,b):
+    return a*b
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+x = mul(a,b)
+
+print("the mul of {0} and {1} is {2}".format(a,b,x))
